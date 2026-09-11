aba680993dc632aab85ff65079282c03b12da300
Dagur Arinbjörn Daníelsson <dagurd@kahoot.com>
2026-09-11 11:17:38 +0200

    Save game

--- Source/MageGame/MageGame.Build.cs
+++ Source/MageGame/MageGame.Build.cs
@@ -14,7 +14,7 @@ public class MageGame : ModuleRules
             "GameplayAbilities", "GameplayTags", "GameplayTasks"
         });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "SPUD" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
